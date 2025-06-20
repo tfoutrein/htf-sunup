de79f75ba72446ce41e75d8e3eb03c3ba4679417
Thomas Foutrein <thomas.foutrein@gmail.com>
2025-06-20 23:52:07 +0200

    fix: simplify lint-staged to only use Prettier

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-4xl font-bold text-center mb-8">
-          Next.js Template
+          Next.js Template 🚀
         </h1>
 
         <div className="mb-8 text-center">
